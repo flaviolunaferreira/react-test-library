76151ac6409600688f9067afc51424b8f1853d74
flavio luna ferreira <flaviolunaferreira@gmail.com>
2021-01-27 17:33:58 -0300

    iniciando testes com react-test-library

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,34 @@
-import { render, screen } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+describe('Verificando se existe componentes do app.js', () => {
+  it('confirmar a label com o nome email', () => {
+    const { getByLabelText } = render(<App />);
+    const textoEmail = getByLabelText(/Email/i);
+    expect(textoEmail).toBeInTheDocument();
+  });
+  it('verificar se tem o campo de input com a label email e tipo email', () => {
+    const { getByLabelText } = render(<App />);
+    const inputEmail = getByLabelText(/Email/i);
+    expect(inputEmail).toBeInTheDocument();
+    expect(inputEmail.type).toBe('email');
+  });
+  it('Verifica se existe 2 botões', () => {
+    const { getAllByRole } = render(<App />);
+    const btn = getAllByRole('button');
+    expect(btn.length).toBe(2);
+  })
+  it('Verifica click do btn enviar, texto aparece na tela', () => {
+    const { getByTestId, getByLabelText } = render(<App />);
+    const emailInput = getByLabelText('Email');
+    const sendButton = getByTestId('id-send');
+    const userEmail = getByTestId('id-email-user');
+    fireEvent.change(emailInput, { target: { value: 'flaviolunaferreira@gmail.com' } });
+    fireEvent.click(sendButton);
+    expect(emailInput.value).toBe('');
+    expect(userEmail.textContent).toBe('Valor: flaviolunaferreira@gmail.com');
+  });
 });
+
+
